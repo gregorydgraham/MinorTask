756162766763966af495b64f731e1f87b896ea64
Gregory Graham <gregory@gregs.co.nz>
2018-07-12 13:31:14 +1200

    Create AccessDeniedComponent to have a consistent response to security issues

---
+++ src/main/java/nz/co/gregs/minortask/components/AccessDeniedComponent.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.minortask.components;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.dom.Element;
+
+/**
+ *
+ * @author gregorygraham
+ */
+@Tag("access-denied-component")
+public class AccessDeniedComponent extends Label implements MinorTaskComponent {
+
+	public AccessDeniedComponent() {
+		setText("Access Denied");
+		setTitle("Access to this task or a required field has been denied either due to security concerns or the task not existing.");
+		getElement().setAttribute("theme", "error");
+	}
+
+	@Override
+	public final void setTitle(String title) {
+		super.setTitle(title);
+	}
+
+	@Override
+	public final Element getElement() {
+		return super.getElement();
+	}
+
+}
