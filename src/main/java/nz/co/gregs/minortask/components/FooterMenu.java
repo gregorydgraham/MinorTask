9964803fc60b3ab497e2f423e15bca8408d5ab62
Gregory Graham <gregory@gregs.co.nz>
2018-07-16 13:42:36 +1200

    Slight improvement of the footer

--- src/main/java/nz/co/gregs/minortask/components/FooterMenu.java
+++ src/main/java/nz/co/gregs/minortask/components/FooterMenu.java
@@ -17,12 +17,9 @@ public class FooterMenu extends VerticalLayout implements MinorTaskComponent {
 //	private final MinorTaskUI ui;
 //	private final Long taskID;
 	public FooterMenu() {
-		final Label label = new Label("MinorTask is a simple system to help you manage projects and tasks.");
+		final Label label = new Label("MinorTask turns every project is a collection of minor tasks.");
 		label.setSizeFull();
 		add(label);
-		final Label label1 = new Label("The key concept is that every project is just a collection of minor tasks.");
-		label1.setSizeFull();
-		add(label1);
 		final Label label2 = new Label("MinorTask provides you the tools to simplify all your tasks and projects into their component minor tasks and complete the successfully.");
 		label2.setSizeFull();
 		add(label2);
