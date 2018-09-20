25e59cea2d7e4e28f3d25cee60e6ba1abf4a2365
Gregory Graham <gregory@gregs.co.nz>
2018-09-20 17:18:09 +1200

    Add searching the descriptions as well as titles

--- src/main/java/nz/co/gregs/minortask/components/tasklists/SearchedTasksList.java
+++ src/main/java/nz/co/gregs/minortask/components/tasklists/SearchedTasksList.java
@@ -7,7 +7,7 @@ package nz.co.gregs.minortask.components.tasklists;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -15,17 +15,31 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import nz.co.gregs.dbvolution.DBQuery;
-import nz.co.gregs.dbvolution.columns.StringColumn;
 import nz.co.gregs.dbvolution.expressions.BooleanExpression;
+import nz.co.gregs.dbvolution.expressions.StringExpression;
+import nz.co.gregs.minortask.components.HasDefaultButton;
 import nz.co.gregs.minortask.datamodel.Task;
 
-public class SearchedTasksList extends AbstractTaskList {
+public class SearchedTasksList extends AbstractTaskList implements HasDefaultButton {
 
 	TextField searchField;
+	Checkbox includeDescriptionOption;
 
 	public SearchedTasksList(Long taskID) {
 		super(taskID);
 		getSearchField();
+	}
+
+	private Checkbox getIncludeDescriptionOption() {
+		if (includeDescriptionOption == null) {
+			includeDescriptionOption = new Checkbox();
+			includeDescriptionOption.setValue(true);
+			includeDescriptionOption.setLabel("Search Description");
+			includeDescriptionOption.addValueChangeListener((event) -> {
+				refreshList();
+			});
+		}
+		return includeDescriptionOption;
 	}
 
 	private TextField getSearchField() {
@@ -63,12 +77,16 @@ public class SearchedTasksList extends AbstractTaskList {
 				Task example = new Task();
 				example.userID.permittedValues(getUserID());
 				DBQuery query = getDatabase().getDBQuery(example);
-				StringColumn column = example.column(example.name);
+				StringExpression column = example.column(example.name);
 				BooleanExpression boolExpr = null;
 				boolExpr = column.searchFor(terms);
-				query.addCondition(boolExpr);
+
+				if (getIncludeDescriptionOption().getValue()) {
+					column = column.append(" ").append(example.column(example.description));
+				}
+				query.addCondition(column.searchFor(terms));
 				query.setSortOrder(column.searchForRanking(terms).descending());
-				System.out.println(query.getSQLForQuery());
+
 				return query.getAllInstancesOf(example);
 			} else {
 				return new ArrayList<Task>();
@@ -97,7 +115,16 @@ public class SearchedTasksList extends AbstractTaskList {
 	@Override
 	protected Component[] getControlsAbove() {
 		Button searchButton = new Button("Search");
-		HorizontalLayout layout = new HorizontalLayout(searchButton, getSearchField());
+		setAsDefaultButton(
+				searchButton,
+				(event) -> refreshList(),
+				(event) -> refreshList()
+		);
+		HorizontalLayout layout = new HorizontalLayout(new Component[]{
+			searchButton,
+			getSearchField(),
+			getIncludeDescriptionOption()
+		});
 		return new Component[]{layout};
 	}
 
