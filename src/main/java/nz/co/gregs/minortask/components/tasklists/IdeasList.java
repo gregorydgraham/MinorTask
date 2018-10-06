19e95d7c393a3406a10cc9eff81c14a13d31c46a
Gregory Graham <gregory@gregs.co.nz>
2018-10-07 12:11:58 +1300

    Reduced the ideas list to only leaf nodes, making the next task easier to pick

---
+++ src/main/java/nz/co/gregs/minortask/components/tasklists/IdeasList.java
@@ -0,0 +1,55 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.minortask.components.tasklists;
+
+import java.sql.SQLException;
+import java.util.List;
+import nz.co.gregs.dbvolution.DBQuery;
+import nz.co.gregs.minortask.Globals;
+import nz.co.gregs.minortask.datamodel.Task;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public class IdeasList extends AbstractTaskList {
+	
+//	private final IdeasPage list;
+
+	public IdeasList(Long taskID) {
+		super(taskID);
+//		this.list = list;
+	}
+
+	@Override
+	protected String getListClassName() {
+		return "ideaslist";
+	}
+
+	@Override
+	protected String getListCaption(List<Task> tasks) {
+		return tasks.size() + " Ideas";
+	}
+
+	@Override
+	protected List<Task> getTasksToList() throws SQLException {
+		Task example = new Task.Project();
+		example.userID.permittedValues(minortask().getUserID());
+		example.startDate.permitOnlyNull();
+		example.preferredDate.permitOnlyNull();
+		example.preferredDate.permitOnlyNull();
+		example.completionDate.permitOnlyNull();
+		Task task = new Task();
+		final DBQuery query = Globals.getDatabase().getDBQuery(example).addOptional(task);
+		// add the leaf requirement
+		query.addCondition(task.column(task.taskID).isNull());
+		query.setSortOrder(example.column(example.name));
+		query.printAllRows();
+		List<Task> tasks = query.getAllInstancesOf(example);
+		return tasks;
+	}
+	
+}
--- src/main/java/nz/co/gregs/minortask/pages/IdeasPage.java
+++ src/main/java/nz/co/gregs/minortask/pages/IdeasPage.java
@@ -5,53 +5,20 @@
  */
 package nz.co.gregs.minortask.pages;
 
+import nz.co.gregs.minortask.components.tasklists.IdeasList;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.router.Route;
-import java.sql.SQLException;
-import java.util.List;
-import nz.co.gregs.dbvolution.DBQuery;
-import nz.co.gregs.minortask.Globals;
-import nz.co.gregs.minortask.components.tasklists.AbstractTaskList;
-import nz.co.gregs.minortask.datamodel.Task;
 
 @Route("ideas")
 public class IdeasPage extends MinorTaskPage {
 
 	@Override
 	public Component getInternalComponent(Long parameter) {
-		return new AbstractTaskList(parameter) {
-			@Override
-			protected String getListClassName() {
-				return "ideaslist";
-			}
-
-			@Override
-			protected String getListCaption(List<Task> tasks) {
-				return tasks.size() + " Ideas";
-			}
-
-			@Override
-			protected List<Task> getTasksToList() throws SQLException {
-				Task example = new Task();
-				example.userID.permittedValues(minortask().getUserID());
-				example.startDate.permitOnlyNull();
-				example.preferredDate.permitOnlyNull();
-				example.preferredDate.permitOnlyNull();
-				example.completionDate.permitOnlyNull();
-				final DBQuery query = Globals.getDatabase().getDBQuery(example);
-				query.setSortOrder(
-						example.column(example.name)
-				);
-				query.printAllRows();
-				List<Task> tasks = query.getAllInstancesOf(example);
-				return tasks;
-			}
-		};
+		return new IdeasList(parameter);
 	}
 
 	@Override
 	public String getPageTitle() {
 		return "MinorTask: Ideas";
 	}
-
 }
