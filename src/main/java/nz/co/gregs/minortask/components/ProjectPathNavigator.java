3eb7087ccd9180438f44b54d7a9efd34cfba8706
Gregory Graham <gregory@gregs.co.nz>
2018-10-12 00:05:19 +1300

    Improved the appearance of the project path when it gets long

--- src/main/java/nz/co/gregs/minortask/components/ProjectPathNavigator.java
+++ src/main/java/nz/co/gregs/minortask/components/ProjectPathNavigator.java
@@ -7,6 +7,7 @@ package nz.co.gregs.minortask.components;
 
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import java.util.Collections;
@@ -19,7 +20,7 @@ import nz.co.gregs.minortask.datamodel.Task;
  * @author gregorygraham
  */
 //@Tag("project-path-navigator")
-public class ProjectPathNavigator extends HorizontalLayout implements RequiresLogin {
+public class ProjectPathNavigator extends Div implements RequiresLogin {
 
 	private final Long taskID;
 
@@ -29,12 +30,16 @@ public class ProjectPathNavigator extends HorizontalLayout implements RequiresLo
 	}
 
 	protected void buildComponent() {
-		add(getButtonForTaskID(null));
+		getStyle().set("overflow", "auto");
+//		add(getButtonForTaskID(null));
 		List<Task> ancestors = MinorTask.getProjectPathTasks(getTaskID(), minortask().getUserID());
 		Collections.reverse(ancestors);
-		for (Task ancestor : ancestors) {
-			add(getButtonForTaskID(ancestor));
-		}
+		ancestors.stream()
+				.filter((ancestor) -> (ancestor != null))
+				.forEachOrdered((ancestor) -> {
+					add(getButtonForTaskID(ancestor));
+				});
+		
 	}
 
 	public Button getButtonForTaskID(Task task) {
