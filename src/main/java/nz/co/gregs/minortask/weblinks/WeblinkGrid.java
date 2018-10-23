2cfb8b2affc94e754b6a765d35f56d4a1e5981e5
Gregory Graham <gregory@gregs.co.nz>
2018-10-24 11:53:14 +1300

    Cleanup

--- src/main/java/nz/co/gregs/minortask/weblinks/WeblinkGrid.java
+++ src/main/java/nz/co/gregs/minortask/weblinks/WeblinkGrid.java
@@ -29,7 +29,6 @@ public class WeblinkGrid extends Div implements RequiresLogin {
 	private Long taskID;
 	private final Grid<Weblink> grid = new Grid<Weblink>();
 	private List<Weblink> allRows;
-//	private WeblinkEditorComponent searcher;
 
 	public WeblinkGrid() {
 	}
@@ -43,9 +42,6 @@ public class WeblinkGrid extends Div implements RequiresLogin {
 	@SuppressWarnings("unchecked")
 	private void makeComponent() {
 		removeAll();
-//		setMargin(false);
-//		setPadding(false);
-//		setSpacing(false);
 		setSizeUndefined();
 		setItems();
 		grid.addClassName("weblink-grid");
@@ -55,26 +51,21 @@ public class WeblinkGrid extends Div implements RequiresLogin {
 		grid.addComponentColumn((source) -> getDescriptionComponent(source)
 		).setFlexGrow(20);
 		grid.addComponentColumn((source) -> getRemoveComponent(source));
-//		searcher = new WeblinkEditorComponent(taskID);
-//		searcher.addWeblinkAddedListener((event) -> {
-//			setItems();
-//		});
 		add(grid);
-//		add(searcher);
 	}
 
 	private Button getRemoveComponent(Weblink source) {
 		final Button button = new Button(new Icon(VaadinIcon.TRASH), (event) -> removePlace(source));
-		button.setEnabled(this.isEnabled()&&!this.isReadOnly());
+		button.setEnabled(this.isEnabled() && !this.isReadOnly());
 		return button;
 	}
 
 	private Component getAnchorComponent(Weblink source) {
 		Component icon = new Icon(VaadinIcon.MAP_MARKER);
-		String value = source.iconURL.getValue();
-		if (value != null && !value.isEmpty()) {
-			System.out.println("URL VALUE: \"" + source.iconURL.getValue() + "\"");
-			icon = new Image(source.iconURL.getValue(), "");
+		String iconURL = source.iconURL.getValue();
+		if (iconURL != null && !iconURL.isEmpty()) {
+			System.out.println("URL VALUE: \"" + iconURL + "\"");
+			icon = new Image(iconURL, "");
 		}
 
 		HorizontalLayout layout = new HorizontalLayout();
@@ -129,7 +120,7 @@ public class WeblinkGrid extends Div implements RequiresLogin {
 		source.description.setValue(value);
 		try {
 			getDatabase().update(source);
-			minortask().chat("Saved");
+			chat("Saved");
 		} catch (SQLException ex) {
 			sqlerror(ex);
 		}
@@ -137,18 +128,15 @@ public class WeblinkGrid extends Div implements RequiresLogin {
 
 	public boolean isReadOnly() {
 		return !isEnabled();
-//		return searcher.isReadOnly(); 
 	}
 
 	public void setReadOnly(boolean readonly) {
-//		searcher.setEnabled(!readonly);
 		grid.setEnabled(!readonly);
 	}
 
 	@Override
 	public void setEnabled(boolean enabled) {
 		getElement().setEnabled(enabled);
-//		searcher.setEnabled(enabled);
 	}
 
 	public void refresh() {
