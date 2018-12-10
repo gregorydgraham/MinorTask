8968cdd7132612201750a8908c7ab061d6e4a3ba
Gregory Graham <gregory@gregs.co.nz>
2018-12-11 00:27:04 +1300

    Added proper listing of the databases within the cluster to the monitor page

--- src/main/java/nz/co/gregs/minortask/pages/ClusterMonitorPage.java
+++ src/main/java/nz/co/gregs/minortask/pages/ClusterMonitorPage.java
@@ -8,7 +8,14 @@ package nz.co.gregs.minortask.pages;
 import com.vaadin.flow.component.Component;
 import nz.co.gregs.minortask.components.ClusterMonitorComponent;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.router.Route;
+import nz.co.gregs.dbvolution.databases.DBDatabase;
+import nz.co.gregs.dbvolution.databases.DBDatabaseCluster;
+import nz.co.gregs.dbvolution.databases.DatabaseConnectionSettings;
+import nz.co.gregs.dbvolution.internal.database.ClusterDetails;
 import nz.co.gregs.minortask.Globals;
 
 /**
@@ -16,22 +23,62 @@ import nz.co.gregs.minortask.Globals;
  * @author gregorygraham
  */
 @HtmlImport("styles/shared-styles.html")
-@Route(value="cluster", layout = MinortaskPushPage.class)
+@StyleSheet("styles/cluster-monitor.css")
+@Route(value = "cluster", layout = MinortaskPushPage.class)
 public class ClusterMonitorPage extends AuthorisedPage {
 
 	private final ClusterMonitorComponent clusterMonitorComponent;
-	
+
 	public ClusterMonitorPage() {
 		clusterMonitorComponent = new ClusterMonitorComponent();
 	}
 
 	@Override
 	protected Component getInternalComponent() {
-		return clusterMonitorComponent;
+		Div div = new Div(clusterMonitorComponent);
+		DBDatabase database = getDatabase();
+		if (database instanceof DBDatabaseCluster) { // never assume its a cluster ;-)
+			DBDatabaseCluster cluster = (DBDatabaseCluster) database;
+			Div clusterDiv = new Div(new Label(cluster.getDatabaseName()));
+			clusterDiv.addClassName("cluster-monitor");
+			div.add(clusterDiv);
+			ClusterDetails details = cluster.getClusterDetails();
+			DatabaseConnectionSettings authoritativeDatabase = details.getAuthoritativeDatabase();
+			Div allDatabasesDiv = new Div();
+			DBDatabase[] allDBs = details.getAllDatabases();
+			for (DBDatabase db : allDBs) {
+				String databaseName = db.getDatabaseName();
+				databaseName = databaseName.isEmpty() ? "Unnamed" : databaseName;
+				final DatabaseConnectionSettings settings = db.getSettings();
+				final Div databaseDescriptionDiv = new Div(
+						new Label(settings.toString()
+								.replaceAll("DATABASECONNECTIONSETTINGS:* *", "")
+								.replaceAll("nz\\.co\\.gregs\\.dbvolution\\.databases\\.", ""))
+				);
+				databaseDescriptionDiv.addClassName("cluster-monitor-database-description");
+				final Div databaseStatusDiv = new Div(
+						new Label(details.getStatusOf(db).name()
+						)
+				);
+				if (settings.equals(authoritativeDatabase)){
+					databaseStatusDiv.add(new Label("AUTHORITATIVE"));
+				}
+				databaseStatusDiv.addClassName("cluster-monitor-database-status");
+				final Div databaseDiv = new Div(
+						new Label(databaseName),
+						databaseDescriptionDiv,
+						databaseStatusDiv
+				);
+				databaseDiv.addClassName("cluster-monitor-database");
+				allDatabasesDiv.add(databaseDiv);
+			}
+			clusterDiv.add(allDatabasesDiv);
+		}
+		return div;
 	}
 
 	@Override
 	public String getPageTitle() {
-		return Globals.getApplicationName()+": Cluster Monitor";
+		return Globals.getApplicationName() + ": Cluster Monitor";
 	}
 }
