c2fb0a121a28dfb16b41132b664a737469110140
Gregory Graham <gregory@gregs.co.nz>
2019-03-02 16:57:12 +1300

    Reduced velocity to 2 decimal places

--- src/main/java/nz/co/gregs/minortask/components/AuthorisedBannerMenu.java
+++ src/main/java/nz/co/gregs/minortask/components/AuthorisedBannerMenu.java
@@ -38,34 +38,34 @@ import nz.co.gregs.minortask.pages.UserProfilePage;
 @Tag("authorised-banner")
 @StyleSheet("styles/authorised-banner.css")
 public class AuthorisedBannerMenu extends SecureDiv implements HasText {
-
+	
 	final Anchor welcomeMessage = new Anchor(Globals.getApplicationURL(), "Welcome");
 	ColleaguesButton colleaguesButton = new ColleaguesButton();
 	ProfileButton profileButton = new ProfileButton();
 	LogoutButton logoutButton = new LogoutButton();
-
+	
 	public AuthorisedBannerMenu() {
 		super();
 		buildComponent();
 		this.addClassName("authorised-banner");
 		this.setId(getStaticID());
 	}
-
+	
 	@Override
 	public final void setId(String id) {
 		super.setId(id);
 	}
-
+	
 	public final void buildComponent() {
 		setSizeUndefined();
-
+		
 		welcomeMessage.addClassName("welcome-message");
-
+		
 		setText("" + Globals.getApplicationName());
-
+		
 		final User user = getCurrentUser();
 		if (user != null) {
-
+			
 			Label counts = new Label("Tasks: ??/##");
 			TaskCounts taskCounts = new TaskCounts();
 			try {
@@ -73,17 +73,17 @@ public class AuthorisedBannerMenu extends SecureDiv implements HasText {
 				owner.queryUserID().permittedValues(user.getUserID());
 				List<TaskCounts> got = getDatabase().get(taskCounts, owner);
 				final TaskCounts gotFirst = got.get(0);
-				counts = new Label("Velocity: "+gotFirst.velocity.stringValue()+" Tasks: " + gotFirst.completed.stringValue() + "/" + gotFirst.created.stringValue());
+				counts = new Label("Velocity: " + gotFirst.velocity.stringValue() + " Tasks: " + gotFirst.completed.stringValue() + "/" + gotFirst.created.stringValue());
 			} catch (SQLException | AccidentalCartesianJoinException | AccidentalBlankQueryException | NoAvailableDatabaseException ex) {
 				sqlerror(ex);
 			}
-
+			
 			SecureDiv defaultImageDiv = new SecureDiv();
 			defaultImageDiv.addClickListener((event) -> {
 				minortask().showProfile();
 			});
 			Component profileImageDiv = defaultImageDiv;
-
+			
 			if (user.profileImage != null) {
 				SizedImageFromDocument image = new SizedImageFromDocument(user.profileImage, 100);
 				image.addClickListener((event) -> {
@@ -91,68 +91,68 @@ public class AuthorisedBannerMenu extends SecureDiv implements HasText {
 				});
 				profileImageDiv = image;
 			}
-
+			
 			profileImageDiv.setId("authorised-banner-profile-image");
-
+			
 			final String welcomeUser = "@" + user.getUsername();
 			Anchor profileAnchor = new Anchor(UserProfilePage.getURL(), welcomeUser);
-
+			
 			colleaguesButton.setId("authorised-banner-colleagues-button");
 			colleaguesButton.addClassName("authorised-banner-button");
-
+			
 			profileButton.setId("authorised-banner-profile-button");
 			profileButton.addClassName("authorised-banner-button");
-
+			
 			logoutButton.setId("authorised-banner-logout-button");
 			logoutButton.addClassName("authorised-banner-button");
-
+			
 			Div left = new Div();
 			left.addClassName("authorised-banner-left");
-
+			
 			Div right = new Div();
 			right.addClassName("authorised-banner-right");
-
+			
 			left.add(profileImageDiv, welcomeMessage);
 			right.add(new Div(new Div(profileAnchor, colleaguesButton, profileButton, logoutButton), counts));
 			add(left, right);
 		}
 	}
-
+	
 	public static String getStaticID() {
 		return "authorised_banner_id";
 	}
-
+	
 	@Override
 	public String getText() {
 		return welcomeMessage.getText();
 	}
-
+	
 	@Override
 	public void setText(String text) {
 		welcomeMessage.setText(text);
 	}
-
+	
 	public void setAllButtonsUnselected() {
 		profileButton.removeClassName("authorised-banner-selected-button");
 		logoutButton.removeClassName("authorised-banner-selected-button");
 	}
-
+	
 	public void setProfileButtonSelected() {
 		profileButton.addClassName("authorised-banner-selected-button");
 	}
-
+	
 	public void setLogoutButtonSelected() {
 		logoutButton.addClassName("authorised-banner-selected-button");
 	}
-
+	
 	public static class TaskCounts extends DBReport {
-
+		
 		Task.Owner user = new Task.Owner();
 		Task task = new Task();
-
+		
 		@DBColumn
 		public DBInteger created = new DBInteger(task.column(task.taskID).count());
-
+		
 		@DBColumn
 		public DBInteger completed = new DBInteger(
 				task.column(task.completionDate)
@@ -160,7 +160,7 @@ public class AuthorisedBannerMenu extends SecureDiv implements HasText {
 						.ifThenElse(1, 0)
 						.sum()
 		);
-
+		
 		@DBColumn
 		public DBNumber velocity = new DBNumber(
 				task.column(task.completionDate)
@@ -172,7 +172,8 @@ public class AuthorisedBannerMenu extends SecureDiv implements HasText {
 						.ifThenElse(1, 0)
 						.sum()
 						.dividedBy(30)
+						.round(2)
 		);
 	}
-
+	
 }
