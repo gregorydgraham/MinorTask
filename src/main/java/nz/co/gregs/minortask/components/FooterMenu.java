201ded48b0307c1d3f56bcb69b0e55d7a849b838
Gregory Graham <gregory@gregs.co.nz>
2019-09-03 17:13:34 +1200

    Simplified footer text

--- src/main/java/nz/co/gregs/minortask/components/FooterMenu.java
+++ src/main/java/nz/co/gregs/minortask/components/FooterMenu.java
@@ -19,9 +19,10 @@ import com.vaadin.flow.component.html.Label;
 public class FooterMenu extends Footer implements MinorTaskComponent {
 
 	public FooterMenu() {
-		final Label label = new Label("MinorTask turns every project into a collection of minor tasks.");
+		addClassName("footer-menu");
+		final Label label = new Label("every project is a collection of minor tasks.");
 		add(label);
-		final Label label2 = new Label("MinorTask provides you with the tools to simplify all your tasks and projects and complete them successfully.");
+		final Label label2 = new Label("MinorTask simplifies your projects so you complete them successfully.");
 		add(label2);
 		setSizeUndefined();
 	}
